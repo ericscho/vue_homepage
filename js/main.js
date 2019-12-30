899763c1566291ea684b088616de27426a27fac2
ericscho <schoneveld@gmail.com>
2019-12-30 11:18:03 +0100

    Develop (#1)
    * First setup vue / skeleton page
    
    * Update index.html
    
    Fixed link to VUE

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+      message: 'Welcome to vultr.schoneveld.com, a digital playground for a developer :)',
+      header: 'Vultr.schoneveld.com'
+  }
+})
